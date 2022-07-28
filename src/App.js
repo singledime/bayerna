f7411420ce67268e5a15080053cb8a8ff7184053
Achu Vyas <hcelab13@outlook.com>
2022-07-28 19:41:20 +0530

    GA 2ND

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import "react-toastify/dist/ReactToastify.css";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-235960528-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
@@ -29,7 +32,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}>
           <Route index element={<Home />} />
-        
         </Route>
       </Routes>
     </BrowserRouter>
